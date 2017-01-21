484b94aa0166cdf01f67fb13a4af7eb9f12c5a60
LAPTOP-C3MQOV66\Victoria <vichuang@seas.upenn.edu>
2017-01-21 17:24:13 -0500

    uploaded java ngram

---
+++ Ngram.java
@@ -0,0 +1,19 @@
+package com.example.victoria.myapplication;
+
+/**
+ * Created by Victoria on 1/21/2017.
+ */
+
+public class Ngram {
+
+    private final int n;
+
+    public Ngram(String corpus, int n) {
+        this.n = n;
+    }
+
+    private void tokenize(String text) {
+        //for ()
+    }
+
+}
