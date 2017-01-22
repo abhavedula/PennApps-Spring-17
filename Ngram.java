1760dd9e92c443e589fea1b681b491a691990466
LAPTOP-C3MQOV66\Victoria <vichuang@seas.upenn.edu>
2017-01-21 20:12:58 -0500

    implemented ngrams

--- Ngram.java
+++ Ngram.java
@@ -1,5 +1,9 @@
 package com.example.victoria.myapplication;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Victoria on 1/21/2017.
  */
@@ -18,6 +22,31 @@ public class Ngram {
             text = text.replace(punc, ' ');
         }
         return text.split("\\s+");
+    }
+
+    private List<Tuple<List<String>, String>> ngrams(String[] tokens) {
+        List<Tuple<List<String>, String>> l = new LinkedList<>();
+        for (int i = 0; i < tokens.length; i++) {
+            List<String> context = new LinkedList<>();
+            for (int j = 0; j < n - 1; j++) {
+                if (i - j - 1 < 0) {
+                    context.add(0, "<START>");
+                } else {
+                    context.add(0, tokens[i - j - 1]);
+                }
+            }
+            l.add(new Tuple<List<String>, String>(context , tokens[i]));
+        }
+        List<String> context = new LinkedList<>();
+        for (int i = 0; i < n - 1; i++) {
+            if (i < tokens.length) {
+                context.add(0, tokens[-i-1]);
+            } else {
+                context.add(0, "<START>");
+            }
+        }
+        l.add(new Tuple<List<String>, String>(context , "<END>"));
+        return l;
     }
 
     private double prob(String context, String token){
